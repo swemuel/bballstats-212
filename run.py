c5dbe70f0b3d88fcf628568a654e4e09c3f70ba7
Sam <swemoulson@gmail.com>
2020-10-12 23:21:27 +1300

    raycast not working

--- run.py
+++ run.py
@@ -8,13 +8,39 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('3j.html')
-    
-@app.route('/stats', methods=['GET'])
-def fetchStats():
+
+@app.route('/stats/points', methods=['GET'])
+def fetchPoints():
 
     con = sqlite3.connect(STATSDB)
     stats = []
     cur = con.execute('SELECT * FROM stats ORDER BY points DESC LIMIT 10')
+
+    for row in cur:
+        stats.append(list(row))
+    con.close()
+
+    return jsonify(stats)
+
+@app.route('/stats/assists', methods=['GET'])
+def fetchAssists():
+
+    con = sqlite3.connect(STATSDB)
+    stats = []
+    cur = con.execute('SELECT * FROM stats ORDER BY assists DESC LIMIT 10')
+
+    for row in cur:
+        stats.append(list(row))
+    con.close()
+
+    return jsonify(stats)
+
+@app.route('/stats/rebounds', methods=['GET'])
+def fetchRebounds():
+
+    con = sqlite3.connect(STATSDB)
+    stats = []
+    cur = con.execute('SELECT * FROM stats ORDER BY rebounds DESC LIMIT 10')
 
     for row in cur:
         stats.append(list(row))
